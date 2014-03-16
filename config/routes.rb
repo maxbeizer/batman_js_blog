1b60ad86d4b86b6079032657a323745825c9fcda
Max Beizer <max.beizer@gmail.com>
2014-03-15 19:20:32 -0500

    Add batamn-generated jazz

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 BatmanJsBlog::Application.routes.draw do
+
+  get "(*redirect_path)", to: "batman#index", constraints: lambda { |request| request.format == "text/html" }
+
   resources :posts do
     resource :comments
   end
